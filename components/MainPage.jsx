d92f8542f52681156fba0a223764aaf91eabfe97
Delia Paun <paundelia23@stud.ase.ro>
2024-04-30 15:00:38 +0300

    add page title

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -48,22 +48,26 @@ const MainPage = () => {
     }
 
     return (
+        <div>
+            <h1 className="mb-4 p-4 text-2xl text-center font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-5xl">
+                <span className="text-transparent bg-clip-text bg-gradient-to-l to-emerald-600 from-teal-400">Gradina Zoologica</span>
+            </h1>
         <div className='p-4 flex flex-wrap gap-4'>
             {data?.map((record) => (
-                <div key={record._id} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div key={record._id} className="max-w-sm p-6 bg-white border border-emerald-500 rounded-lg shadow-lg dark:bg-black-800 dark:border-black-700">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         {record.name}
                     </h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <p className="mb-3 font-normal text-black-700 dark:text-gray-400">
                         {record.description}
                     </p>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <p className="mb-3 font-normal text-black-700 dark:text-gray-400">
                         {record.country}
                     </p>
                     <button 
                         type="button" 
                         onClick={() => handleEditRecord(record._id)}
-                        className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                         Update
                     </button>
                     <button 
@@ -74,6 +78,7 @@ const MainPage = () => {
                     </button>
                 </div>
             ))};
+        </div>
         </div>
     );
 };
